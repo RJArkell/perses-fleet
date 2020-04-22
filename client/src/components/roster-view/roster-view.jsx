2d347f71421d291179736a77b9b97d62e6843ba5
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-04-22 17:26:05 -0400

    Updated RosterView

--- client/src/components/roster-view/roster-view.jsx
+++ client/src/components/roster-view/roster-view.jsx
@@ -11,25 +11,12 @@ export class RosterView extends React.Component {
     return (
       <Container fluid className="p-5 text-center" >
         <Card>
-          <Card.Header className="header"><h3>Command Staff</h3></Card.Header>
+          <Card.Header className="header"><h3>Roster</h3></Card.Header>
           <Row noGutters={true} className="h-100">
             {users.map(u =>
-              <Col xl={2} lg={3} md={4} sm={6}>
+              <Col xl={3} lg={4} md={6} sm={12}>
                 <UserCard key={u._id} u={u} />
               </Col>)}
-          </Row>
-        </Card>
-
-        <Card className="mt-4">
-          <Card.Header className="header border border-dark"><h3>Junior Ranks</h3></Card.Header>
-          <Row noGutters={true} className="h-100">
-
-          </Row>
-        </Card>
-
-        <Card className="mt-4">
-          <Card.Header className="header"><h3>Recruits</h3></Card.Header>
-          <Row noGutters={true} className="h-100">
           </Row>
         </Card>
       </Container >
