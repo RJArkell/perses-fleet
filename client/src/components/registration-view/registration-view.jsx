7373146c1ee8292ad5b0a16da3941d0930ad88fd
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-04-25 17:17:04 -0400

    Updated registration

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -3,7 +3,6 @@ import Button from "react-bootstrap/Button";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
 
 export function RegistrationView(props) {
   const [username, setUsername] = useState('');
@@ -30,52 +29,51 @@ export function RegistrationView(props) {
   };
 
   return (
-    <Card style={{ width: "40%", backgroundColor: 'lightgrey' }}>
-      <Card.Body>
-        <Card.Title><h1>Edge of Umbra</h1></Card.Title>
-        <Card.Text>Enter your information to create a new account</Card.Text>
-        <Form>
-          <Form.Group controlId="regUsername">
-            <Form.Label>Username - Must be at least 5 characters long</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="regPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="regEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="example@email.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="regRank">
-            <Form.Label>Rank</Form.Label>
-            <Form.Control
-              type="date"
-              value={rank}
-              onChange={(e) => setRank(e.target.value)}
-            />
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-          <Link to={`/login`}>
-            <Button variant="secondary">Login with existing account</Button>
-          </Link>
-        </Form>
-      </Card.Body>
-    </Card>
+    <Container className="loginform text-center" >
+      <Card>
+        <Card.Body>
+          <Card.Title><h1>Edge of Umbra</h1></Card.Title>
+          <Card.Text>Enter your information to create a new account</Card.Text>
+          <Form>
+            <Form.Group controlId="regUsername">
+              <Form.Label>Username - Must be at least 5 characters long</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="regPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="regEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="example@email.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="regRank">
+              <Form.Label>Rank</Form.Label>
+              <Form.Control
+                type="date"
+                value={rank}
+                onChange={(e) => setRank(e.target.value)}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
