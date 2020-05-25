298d384a3f708e52067ee0d657e9f816e50796fb
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-05-25 17:02:09 -0400

    Updated Fleet page

--- client/src/components/fleet-view/fleet-view.jsx
+++ client/src/components/fleet-view/fleet-view.jsx
@@ -32,6 +32,11 @@ export class FleetView extends React.Component {
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
+                      <p>STATUS:  Active</p>
+                    </Card.Text>
+                  </Row>
+                  <Row className="mt-2">
+                    <Card.Text>
                       <p>DETAILS:  The Aegis Hammerhead is a light, fast patrol ship bristling with six manned quad-linked turrets for optimal anti-fighter and anti-ordnance defense. It is also suited to support larger capital ships in a fleet or act as a flagship for fighter groups.</p>
                     </Card.Text>
                   </Row>
@@ -70,6 +75,11 @@ export class FleetView extends React.Component {
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
+                      <p>STATUS:  Active</p>
+                    </Card.Text>
+                  </Row>
+                  <Row className="mt-2">
+                    <Card.Text>
                       <p>DETAILS:  The Anvil Carrack features reinforced fuel tanks for long-duration flight, an advanced jump drive, and a dedicated computer core for jump charting operations. Originally a military exclusive, the Carrack is now available for civilian use. On-board accomodations include crew medical and repair facilities, and a mapping-oriented sensor suite.</p>
                     </Card.Text>
                   </Row>
@@ -99,22 +109,27 @@ export class FleetView extends React.Component {
                 <Col xs={7} className="mt-5 pr-5">
                   <Row>
                     <Card.Text>
-                      <p>MODEL:  </p>
+                      <p>MODEL:  RSI Polaris</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>CAPTAIN:  </p>
+                      <p>CAPTAIN:  N/A</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>ROLE:  </p>
+                      <p>ROLE:  Corvette</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>DETAILS:  </p>
+                      <p>STATUS:  In Production</p>
+                    </Card.Text>
+                  </Row>
+                  <Row className="mt-2">
+                    <Card.Text>
+                      <p>DETAILS:  The RSI Polaris is a ship in the concept stage and will be filling the void between larger capital sized ships and non-capital ships that had been created when the original Idris grew in size significantly during production. At the length of around 155 meters the Polaris will be a Corvette. It has been mentioned that it will have a military focus.</p>
                     </Card.Text>
                   </Row>
                 </Col>
@@ -137,22 +152,27 @@ export class FleetView extends React.Component {
                 <Col xs={7} className="mt-5 pl-5">
                   <Row>
                     <Card.Text>
-                      <p>MODEL:  </p>
+                      <p>MODEL:  Aegis Idris-K</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>CAPTAIN:  </p>
+                      <p>CAPTAIN:  N/A</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>ROLE:  </p>
+                      <p>ROLE:  Frigate</p>
                     </Card.Text>
                   </Row>
                   <Row className="mt-2">
                     <Card.Text>
-                      <p>DETAILS:  </p>
+                      <p>STATUS:  In Production</p>
+                    </Card.Text>
+                  </Row>
+                  <Row className="mt-2">
+                    <Card.Text>
+                      <p>DETAILS:  The Aegis Idris-K is an aftermarket special edition of the Idris-P that features additional equipments tailored to dealing with the swarm threat of carrier-type ships. Compared to the Idris-P, Idris-K has an additional size 10 laser beam, four point defense turrets that replaced the remote turrets, and a missile turret that replaced a manned turret.</p>
                     </Card.Text>
                   </Row>
                 </Col>
