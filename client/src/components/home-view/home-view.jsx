10e2ecf833611214527dc179e2af559c14be86e9
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-06-04 23:47:02 -0400

    Update home-view.jsx

--- client/src/components/home-view/home-view.jsx
+++ client/src/components/home-view/home-view.jsx
@@ -60,7 +60,6 @@ export class HomeView extends React.Component {
               <Card.Header className="header text-center"><h3>About Perses Fleet</h3></Card.Header>
               <Card.Body>
                 <Card.Text>
-                  Text
                 </Card.Text>
               </Card.Body>
             </Card>
