70cc552975ac129003b958912474c02d8382bb60
RJArkell <51424552+RJArkell@users.noreply.github.com>
2020-06-10 13:53:03 -0400

    Navbar fixes

--- client/src/components/menubar/menubar.jsx
+++ client/src/components/menubar/menubar.jsx
@@ -6,27 +6,27 @@ export class Menubar extends React.Component {
 
   render() {
     return (
-      <Navbar path="/" className="menubar" expand="lg">
+      <Navbar path="/" className="menubar" expand="xl">
         <Navbar.Brand href="/">
-          <h2 className="menuheader"><img className="navlogo pr-2" src={"https://i.ibb.co/Trr8BWD/navlogo.png"}></img>CSG-PERSES</h2>
+          <div className="menuheader"><img className="navlogo pr-2" src={"https://i.ibb.co/Trr8BWD/navlogo.png"}></img><h2 className="pb-2">CSG-PERSES</h2></div>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" className="hamburger" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto pr-5">
-            <Nav.Link href="/" className="menulink">Main</Nav.Link>
-            <Nav.Link href="/news" className="menulink">News</Nav.Link>
-            <Nav.Link href="/fleet" className="menulink">Fleet</Nav.Link>
-            <Nav.Link href="/media" className="menulink">Media</Nav.Link>
-            <Nav.Link target="_blank" href="https://robertsspaceindustries.com/spectrum/community/PERSES" className="menulink">Forum</Nav.Link>
-            <Nav.Link target="_blank" href="https://robertsspaceindustries.com/orgs/PERSES" className="menulink">Enlist</Nav.Link>
-            <NavDropdown title={<div className="menulink">Restricted Area</div>} id="basic-nav-dropdown">
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="ml-auto pr-5 text-right pt-2">
+            <Nav.Link href="/" className="menulink mt-2">Main</Nav.Link>
+            <Nav.Link href="/news" className="menulink mt-2">News</Nav.Link>
+            <Nav.Link href="/fleet" className="menulink mt-2">Fleet</Nav.Link>
+            <Nav.Link href="/media" className="menulink mt-2">Media</Nav.Link>
+            <Nav.Link target="_blank" href="https://robertsspaceindustries.com/spectrum/community/PERSES" className="menulink mt-2">Forum</Nav.Link>
+            <Nav.Link target="_blank" href="https://robertsspaceindustries.com/orgs/PERSES" className="menulink mt-2">Enlist</Nav.Link>
+            <NavDropdown title={<div className="menulink mt-2">Restricted Area</div>} id="basic-nav-dropdown">
               <NavDropdown.Item href="/dashboard" className="menulink">Dashboard</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="/operations" className="menulink">Operations</NavDropdown.Item>
               <NavDropdown.Item href="/roster" className="menulink">Roster</NavDropdown.Item>
               <NavDropdown.Item href="/objectives" className="menulink">Objectives</NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link target="_blank" href="https://discord.gg/pSYvzsz" ><img src="https://i.ibb.co/XCvDY5F/discord.png" className="icon" /></Nav.Link>
+            <Nav.Link target="_blank" href="https://discord.gg/pSYvzsz" ><img src="https://i.ibb.co/XCvDY5F/discord.png" className="icon mt-1" /></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
